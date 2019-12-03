68ac8c80d2b6fe40469dc6cf7201afb375315294
kewaechter <kewaechter@users.noreply.github.com>
2019-12-03 09:41:37 -0700

    Create pulling_files.py

---
+++ pulling_files.py
@@ -0,0 +1,22 @@
+import os
+
+"""
+    For the given path, get the List of all files in the directory tree 
+'''
+def retrieve_all_filenames(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+    return allFiles
+    
+    """
+    OR
+    """
+listOfFiles = list()
+for (dirpath, dirnames, filenames) in os.walk(dirName):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
